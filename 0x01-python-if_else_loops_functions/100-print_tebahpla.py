47c9734e7391d33d764cfb54c7612d78beb50f69
meyota <meyota08@gmail.com>
2022-07-03 07:54:00 -0700

    Task 100

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 100-print_tebahpla.py
+x = 0
+for y in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(y - x)), end="")
+    x = 32
+    if x == 0:
+    else 0:
+

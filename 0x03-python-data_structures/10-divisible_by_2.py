6552340fbed7257b26a88c86cd0109a808d4cd3b
meyota <meyota08@gmail.com>
2022-07-04 05:50:35 -0700

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 
 def divisible_by_2(my_list=[]):
     multiples = []
-    for i in my_list:
-        if i % 2 == 0:
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
             multiples.append(True)
         else:
             mutiples.append(False)

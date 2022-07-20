cfc5db6dbe4400fc63b8263b3930ff4ac9e143f6
meyota <meyota08@gmail.com>
2022-07-20 13:44:51 -0700

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """A module for square."""
 
+
 class Square:
     """square class with its size"""
 
     def __init__(self, size=0):
-        if (type(size) is not int):
+        if (type(size) != int):
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")

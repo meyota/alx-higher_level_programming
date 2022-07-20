6b521839c3b66dea9d6ec6c1926606ab10018585
meyota <meyota08@gmail.com>
2022-07-20 01:44:08 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines based on 0-squre.py
+"""
+
+class Square():
+    """ Square class """
+    def __init__(self, size):
+        """ class initialization """
+        self.__size = size

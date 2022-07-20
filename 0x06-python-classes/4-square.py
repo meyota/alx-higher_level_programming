2c30e46a6dbc5558c2c16ac61c71bebaafeac283
meyota <meyota08@gmail.com>
2022-07-20 13:50:46 -0700

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """class Square that defines a square"""
 
+
 class Square:
     """square class with it's size"""
 
@@ -13,7 +14,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if (type(value) is not int):
+        if (type(value) != int):
             raise TypeError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")

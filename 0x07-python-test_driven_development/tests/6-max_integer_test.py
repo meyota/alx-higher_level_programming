9179b293f13e20b28edaec0fe1f40b68403d2024
meyota <meyota08@gmail.com>
2022-07-31 08:54:19 -0700

    Task 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unittests for max_integer"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ tests for max_integer"""
+
+    def test_ordered(self):
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
+
+    def test_none(self):
+        """tests empty input"""
+        self.assertEqual(max_integer(), None)
+
+    def test_unordered(self):
+        """tests unordered list"""
+        unordered = [1, 2, 4, 3]
+        self.assertEqual(max_integer(unordered), 4)
+
+    def test_neg(self):
+        """tests negative"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+if __name__ == "__main__":
+    unittest.main()

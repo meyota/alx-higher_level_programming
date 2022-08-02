bf6bc22001bf9c81129aa1335ec0d2631a7ee52e
meyota <meyota08@gmail.com>
2022-08-02 06:26:27 -0700

    Task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define text append."""
+
+def append_write(filename="", text=""):
+    """Append string to the end of UTF8 text file.
+
+    Arrgs:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    returns:
+        number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)

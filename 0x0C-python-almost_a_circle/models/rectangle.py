3f780b9f5e44126e9a69e72e1a86d087de317b35
meyota <meyota08@gmail.com>
2022-08-08 23:44:33 -0700

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""rectangle
+"""
+    rectangle
 """
 from models.base import Base
 
@@ -9,8 +10,36 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        '''
+            returning private attribute.
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+            setting private attribute.
+        '''
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+            returning private attribute.
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+            setting private attribute.
+        '''
+        self.__height = value

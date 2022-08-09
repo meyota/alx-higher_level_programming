88c40d5903b6c0084d4014640458e7acb59bd06e
meyota <meyota08@gmail.com>
2022-08-09 05:57:19 -0700

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!usr/bin/python3
+'''
+    class Square
+'''
+from models.rectangle import Rectangle
+
+
+class Square(Recatngle):
+    '''Inherits from rectangle.'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        '''
+            returns formatted representation.
+        '''
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                        self.x,
+                                                        self.y,
+                                                        self.width)

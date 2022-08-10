b6f435304647f4c1992ebd60ff10d2c58092db34
meyota <meyota08@gmail.com>
2022-08-10 12:23:54 -0700

    Base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''
+            returns json representation
+        '''
+        if list_dictionaries is None:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)

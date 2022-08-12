0a543a4eef286740367c08a1ebe6dd5a04b38621
meyota <meyota08@gmail.com>
2022-08-12 01:20:30 -0700

    unit_test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Unittest for class Rectangle
+"""
+    Unittest for Base class
 """
 import unittest
-import os
-from io import StringIO
-from unittest.mock import patch
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestRectangle(unittest.TestCase):

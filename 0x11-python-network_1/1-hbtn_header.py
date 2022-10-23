60b6dc90673f69c7830d54290922fb445534dced
meyota <meyota08@gmail.com>
2022-10-23 11:34:06 +0000

    Response_header_value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
+
+from sys import argv
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    request = Request(url)
+    with urlopen(request) as res:
+        print(dict(res.headers).get("X-Request-Id"))

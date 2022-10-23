d4c6e15288c370b64ca3705b7542d2d768855cf0
meyota <meyota08@gmail.com>
2022-10-23 08:42:47 -0700

    What_my_status_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == '__main__':
     res = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(res.txt)))
-    print("\t- content: {}".format(res.txt))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))

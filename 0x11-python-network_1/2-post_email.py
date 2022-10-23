7bbd1449ec6aa6d5c10186d66981345cc8d99b6d
meyota <meyota08@gmail.com>
2022-10-23 08:10:06 -0700

    POST_an_emai

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ and displays the body of the response (decoded in utf-8)
 
 import sys
 import urllib.request
-
+import urllib.parse
 if __name__ == '__main__':
     url = sys.argv[1]
     value = {"email": sys.argv[2]}

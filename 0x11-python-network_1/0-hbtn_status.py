b7a2f827b2c9ffb13984aa1317ae488bc3aba8ea
meyota <meyota08@gmail.com>
2022-10-23 10:47:04 +0000

    ntk

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Script that fetches https://intranet.hbtn.io/status"""
+
+import urllib.request as request
+
+if __name__== "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))

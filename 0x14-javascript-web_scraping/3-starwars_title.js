90ef1dfbb2b52280d8d76f9807d8264b18b8e57f
meyota <meyota08@gmail.com>
2022-11-15 19:21:49 +0000

    Star_wars_movie_title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,8 @@ const request = require('request');
 const id = process.argv[2];
 const endpoint = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(endpoint, (err, response, body) => {
-  if (err) {
+request(endpoint, (error, response, body) => {
+  if (error) {
     console.error(error);
   } else {
     console.log(JSON.parse(body).title);
